902de251853acdafdeabc7c254032574bb90047c
RipInPease <svorrevfjelde@gmail.com>
2025-05-16 21:47:35 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#[derive(Clone)]
+#[derive(Debug)]
 struct Tile {
     safe: bool,
     surrounding_bombs: u8,
@@ -10,6 +12,12 @@ struct Tile {
 fn main() {
     init_board(32, 16, 99);
     let mut tiles: Vec<Vec<Tile>> = Vec::new();
+    let width = 2;
+    let height = 2;
+
+    tiles = vec![vec![Tile{safe: false, surrounding_bombs: 0, flagged: false, surrounding_flags: 0}; height]; width];
+    println!("{:?}", tiles[1][0]);
+    
 }
 
 fn init_board (width: u8, height: u8, bombs: u8){
