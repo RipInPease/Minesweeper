e5995be0803ecc33c98a65c1ac21764755651ab2
RipInPease <svorrevfjelde@gmail.com>
2025-05-17 19:27:47 +0200

    Still testing

--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,25 @@ struct Tile {
 
 
 fn main() {
-    init_board(32, 16, 99);
     let mut tiles: Vec<Vec<Tile>> = Vec::new();
     let width = 2;
     let height = 2;
 
-    tiles = vec![vec![Tile{safe: false, surrounding_bombs: 0, flagged: false, surrounding_flags: 0}; height]; width];
-    println!("{:?}", tiles[1][0]);
+    init_board(2, 2, 27);
     
 }
 
-fn init_board (width: u8, height: u8, bombs: u8){
+fn init_board (width: u8, height: u8, bombs: usize){
     use rand::random_range;
     let density = bombs as f32 / ((width as f32) * (height as f32));
+    let x = random_range(0.0..1.0);
 
+    let tiles = vec![vec![Tile{safe: false, surrounding_bombs: 0, flagged: false, surrounding_flags: 0}; width as usize]; height as usize];
+
+    for y in (0..height) {
+        for x in (0..width) {
+            println!("{:?}", tiles[x as usize][y as usize]);
+        }
+    }
 }
 
